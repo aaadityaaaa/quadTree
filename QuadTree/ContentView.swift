f11bf775052f397ce8face2528bd2a15f22b2a12
aaadityaaaa <aaadityaaazz@gmail.com>
2022-05-31 11:55:33 +0530

    initial commit

---
+++ QuadTree/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuadTree
+//
+//  Created by Aaditya Singh on 31/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuadTree/QuadTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuadTreeApp.swift
+//  QuadTree
+//
+//  Created by Aaditya Singh on 31/05/22.
+//
+
+import SwiftUI
+
+@main
+struct QuadTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
